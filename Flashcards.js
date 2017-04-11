4b30c2f61773b276d65276f1938d0cda92946b77
Steven Fox <sfrunner1188@gmail.com>
2017-04-11 09:54:54 -0700

    Added BasicCard Constructor and Tested

--- Flashcards.js
+++ Flashcards.js
@@ -1,1 +1,12 @@
-console.log("hello");
+var BasicCard = function (Front, Back) {
+    this.front = Front;
+    this.back = Back;
+}
+var president = new BasicCard("Who is the current President?", "Donald Trump");
+console.log(president.front);
+console.log(president.back);
+
+
+var ClozeCard = function (Text, Cloze) {
+
+}
